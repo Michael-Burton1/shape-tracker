3a1e5d17a8b9b6804e2fefd55072f81ab0371788
Michael Burton <micbur1@gmail.com>
2021-02-01 14:57:45 -0800

    add all files for json

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './triangle.js';
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
